706c06521b61fa7ae589c6d721cb5876e0e01ad1
vanvanich <panahno13@gmail.com>
2024-10-19 11:47:43 +0300

    add main page with search form for contact address

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
+import {Box, FormControl, TextField,} from "@mui/material";
 
 function App() {
 
-  return (
-    <>
-      <p>DADADA</p>
-    </>
-  )
+    const [contractAddress, setContractAddress] = useState('');
+
+    return (
+        <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
+            <div>
+                <FormControl fullWidth sx={{m: 1}} variant="standard">
+                    <TextField
+                        sx = {{
+                            width: '50ch'
+                        }}
+                        label="Input contract address"
+                        id="ca"
+                        onChange={(event) => {
+                            setContractAddress(event.target.value);
+                        }}
+                    />
+                </FormControl>
+            </div>
+        </Box>
+    )
 }
 
 export default App
